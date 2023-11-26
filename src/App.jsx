dafdda1e46ccfde50a1d5feed820e901631b0e24
Julie <j.prunaret@gmail.com>
2023-11-26 14:21:06 +0100

    install - Tailwinds scrollbar + config + style fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Title from "./components/Title";
 
 function App() {
   return (
-    <div className="h-[100vh] bg-[color:var(--bg-color)]">
+    <div className="h-[100vh] bg-[color:var(--bg-color)] overflow-hidden">
       <NavBar />
-      <aside className="custom-scrollbar relative z-10 min-h-full flex-1 origin-left bg-bg text-light-elements shadow-xl transition-all lg:w-1/3 w-4/5 overflow-y-scroll">
+      <aside className="h-32 scrollbar scrollbar-thumb-[color:var(--primary-color)] scrollbar-track-[color:var(--bg-color)] relative z-10 min-h-full flex-1 origin-left bg-[color:var(--bg-color)] text-light-elements shadow-xl transition-all lg:w-1/3 w-4/5 overflow-y-scroll">
         <Title />
         <div className="w-full border border-[#3d3d3d]"></div>
         <Filter />
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -3,12 +3,27 @@ import Card from "./Card";
 
 function SearchResults() {
   return (
-    <div className="flex w-full flex-col gap-6 p-4 text-[color:var(--light-elements)]">
+    <div className="flex w-full flex-col gap-6 p-4 text-[color:var(--light-elements)] mb-20">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl font-semibold ">Explorer</h2>
         <Button title="Filter" icon="✍️" isReverse={true} />
       </div>
       <ul className="flex flex-col gap-6">
+        <Card
+          adress="28 Rue Mathieu Donnart, 29200 Brest, France"
+          name="Nom du bar"
+          type="Bar"
+        />
+        <Card
+          adress="28 Rue Mathieu Donnart, 29200 Brest, France"
+          name="Nom du bar"
+          type="Bar"
+        />
+        <Card
+          adress="28 Rue Mathieu Donnart, 29200 Brest, France"
+          name="Nom du bar"
+          type="Bar"
+        />
         <Card
           adress="28 Rue Mathieu Donnart, 29200 Brest, France"
           name="Nom du bar"
